fedc431b635a58bbad042a423618e6b4e1633871
namanb009 <35342183+namanb009@users.noreply.github.com>
2020-02-28 23:42:06 +0530

    Added engine class + Browse button function

--- main.py
+++ main.py
@@ -1,5 +1,53 @@
 from tkinter import *
+from tkinter import filedialog
+import os
 
+
+class Engine():
+    add = []  #stores selected file addresses
+    filename = []  #stores selected file names
+    doc = None  #doc object to work on current word file
+    docf=None   #Final doc object
+    n=0 #number of lines in the doc file
+
+    def __init__(self):
+        pass
+
+    def inputfile(self, root):
+        del self.add[::], self.filename[::]
+        
+        s = filedialog.askopenfilenames()
+
+        #seperating filename from the entire addresses
+        for i in s:
+            i = i[::-1]
+            temp = 0
+            
+            for j in range(len(i)):
+                if i[j] == "/":
+                    temp = j
+                    break
+            
+            temp = len(i) - temp
+            i = i[::-1]
+            
+            self.add.append(i[:temp])
+            self.filename.append(i[temp:])
+        
+        self.N=len(self.add)
+
+        l21.config(text=str(self.N))
+
+        temp=""
+        for i in self.filename:
+            temp=temp+ i + "\n"
+        temp=temp[:-1]
+
+        l31.config(text=temp)
+
+
+
+e=Engine()
 root = Tk()
 root.title("Document Formatter and Corrector")
 root.geometry("800x400")
@@ -15,7 +63,7 @@ l0=Label(f2,text="")
 l0.grid(row=0,columnspan=2)
 
 
-b1 = Button(f2, text="Browse files", command=lambda: e.inputfile(root), width=40, height=1)
+b1 = Button(f2, text="Browse files",command= lambda:e.inputfile(root), width=40, height=1)
 b1.grid(row=1, column=0, padx=2, pady=2, sticky=W, columnspan=2)
 b1.config(relief="solid")
 
@@ -23,7 +71,7 @@ l20=Label(f2,text="Number of files selected: ")
 l20.grid(row=2,column=0,padx=2,pady=2,sticky=W)
 
 l21=Label(f2,text="")
-l21.grid(row=2,column=1,padx=2,pady=2,sticky=W)
+l21.grid(row=2,column=1,padx=2,pady=2,sticky=W,columnspan=2)
 
 l30=Label(f2,text="File Selected:")
 l30.grid(row=3,column=0,padx=2,pady=2,sticky=NW)
@@ -31,12 +79,12 @@ l30.grid(row=3,column=0,padx=2,pady=2,sticky=NW)
 l31=Label(f2,text="")
 l31.grid(row=3, column=1, padx=2, pady=2, sticky=W)
 
-
 b41=Button(f2,text="Go!",command=None,width=40,height=1)
 b41.grid(row=4, column=0, padx=2, pady=2, sticky=W, columnspan=2)
 b41.config(relief="solid")
 
 l50 = Label(f2, text="")
 l50.grid(row=5, column=0, padx=2, pady=2, sticky=W)
+
 
 root.mainloop()
