8f0657d0fa86fd6c9cd335303dbb59bb762ffd14
Naman Bansal <35342183+namanb009@users.noreply.github.com>
2020-02-29 14:16:16 +0530

    Minor corrections to previous commit

--- main.py
+++ main.py
@@ -19,33 +19,37 @@ class Engine():
         
         s = filedialog.askopenfilenames()
 
-        #seperating filename from the entire addresses
-        for i in s:
-            i = i[::-1]
-            temp = 0
-            
-            for j in range(len(i)):
-                if i[j] == "/":
-                    temp = j
-                    break
-            
-            temp = len(i) - temp
-            i = i[::-1]
-            
-            self.add.append(i[:temp])
-            self.filename.append(i[temp:])
+        if len(s) == 0:
+            l21.config(text="0")
+            l31.config(text="None")
         
-        self.N=len(self.add)
+        else:
+            #seperating filename from the entire addresses
+            for i in s:
+                i = i[::-1]
+                temp = 0
+            
+                for j in range(len(i)):
+                    if i[j] == "/":
+                        temp = j
+                        break
+            
+                temp = len(i) - temp
+                i = i[::-1]
+            
+                self.add.append(i[:temp])
+                self.filename.append(i[temp:])
+        
+            self.N=len(self.add)
 
-        l21.config(text=str(self.N))
+            l21.config(text=str(self.N))
 
-        temp=""
-        for i in self.filename:
-            temp=temp+ i + "\n"
-        temp=temp[:-1]
+            temp=""
+            for i in self.filename:
+                temp=temp+ i + "\n"
+            temp=temp[:-1]
 
-        l31.config(text=temp)
-
+            l31.config(text=temp)
     
     def enginecontrol(self):
         for i in range(self.N):
@@ -71,35 +75,39 @@ root = Tk()
 root.title("Document Formatter and Corrector")
 root.geometry("800x400")
 
+#Left blue frame
 f1 = Frame(root, width=200, height=400)
 f1.config(background= "#b7e9fd")
 f1.pack(side=LEFT)
 
+#Right main frame
 f2 = Frame(root, width=400, height=400)
 f2.pack()
 
-l0=Label(f2,text="")
-l0.grid(row=0,columnspan=2)
+l0=Label(f2,text="")    #Blank label
+l0.grid(row=0,column=0,columnspan=3)
 
 
-b1 = Button(f2, text="Browse files",command= lambda:e.inputfile(root), width=40, height=1)
-b1.grid(row=1, column=0, padx=2, pady=2, sticky=W, columnspan=2)
+#Browse button
+b1 = Button(f2, text="Browse files",command= lambda:e.inputfile(root), width=60, height=1)
+b1.grid(row=1, column=0, padx=2, pady=2, sticky=W, columnspan=3)
 b1.config(relief="solid")
 
+#Metadata1
 l20=Label(f2,text="Number of files selected: ")
 l20.grid(row=2,column=0,padx=2,pady=2,sticky=W)
+l21=Label(f2,text="0")
+l21.grid(row=2,column=1,padx=2,pady=2,sticky=W,columnspan=3)
 
-l21=Label(f2,text="")
-l21.grid(row=2,column=1,padx=2,pady=2,sticky=W,columnspan=2)
-
+#metadata2
 l30=Label(f2,text="File Selected:")
-l30.grid(row=3,column=0,padx=2,pady=2,sticky=NW)
+l30.grid(row=3,column=0,padx=2,pady=2,sticky=W)
+l31=Label(f2,text="None")
+l31.grid(row=3, column=1, padx=2, pady=2,sticky=W)
 
-l31=Label(f2,text="")
-l31.grid(row=3, column=1, padx=2, pady=2, sticky=W)
-
-b41=Button(f2,text="Go!",command=lambda:e.enginecontrol(),width=40,height=1)
-b41.grid(row=4, column=0, padx=2, pady=2, sticky=W, columnspan=2)
+#Go button
+b41=Button(f2,text="Go!",command=lambda:e.enginecontrol(),width=60,height=1)
+b41.grid(row=4, column=0, padx=2, pady=2, sticky=W, columnspan=3)
 b41.config(relief="solid")
 
 l50 = Label(f2, text="")
